a65604e9ade947a7b0edeed6c17db789b54c82f3
Wyatt Kirby <wyatt@apsis.io>
2019-05-01 18:22:32 -0700

    Update styles throughout

--- pages/index.js
+++ pages/index.js
@@ -15,56 +15,46 @@ export default () => {
     <Layout pageTitle="Welcome">
       <Hero />
       <Section id="copy">
-        <Container className="text-center">
-          <Row className="mb-3">
-            <Col md={{ size: 6, offset: 3 }}>
-              <Title size="4">Let's Play Ball</Title>
-              <p className="lead">
-                Hot Routes: Virtual Football brings the fun and excitement of
-                America's favorite game to a VR headset near you. Take the field
-                and test your arm against an increasingly difficult set of
-                routes.
-              </p>
-              <hr />
+        <Container className="container-narrow text-center">
+          <Title size="4">Let's Play Ball</Title>
+          <p className="lead">
+            Hot Routes: Virtual Football brings the fun and excitement of
+            America's favorite game to a VR headset near you. Take the field and
+            test your arm against an increasingly difficult set of routes.
+          </p>
+          <hr />
 
-              <div className="images">
-                <img
-                  src="/static/img/logos/unity.png"
-                  alt="Made with Unity"
-                  className="img-fluid d-inline-block mx-3"
-                />
-                <img
-                  src="/static/img/logos/vive.png"
-                  alt="HTC Vive"
-                  className="img-fluid d-inline-block mx-3"
-                />
-                <img
-                  src="/static/img/logos/steam.png"
-                  alt="Made with Steam VR"
-                  className="img-fluid d-inline-block mx-3"
-                />
-              </div>
+          <div className="images">
+            <img
+              src="/static/img/logos/unity.png"
+              alt="Made with Unity"
+              className="img-fluid d-inline-block mx-3"
+            />
+            <img
+              src="/static/img/logos/vive.png"
+              alt="HTC Vive"
+              className="img-fluid d-inline-block mx-3"
+            />
+            <img
+              src="/static/img/logos/steam.png"
+              alt="Made with Steam VR"
+              className="img-fluid d-inline-block mx-3"
+            />
+          </div>
 
-              <hr />
+          <hr />
 
-              <Subtitle priority="2" size="4" className="mb-3">
-                Stay up to Date
-              </Subtitle>
+          <Subtitle priority="2" size="4">
+            Stay up to Date
+          </Subtitle>
 
-              <MailChimpForm url="https://twitter.us14.list-manage.com/subscribe/post?u=c995eaf6129ecf4b13d8ae469&id=dd2d98db0f" />
-            </Col>
-          </Row>
+          <MailChimpForm url="https://twitter.us14.list-manage.com/subscribe/post?u=c995eaf6129ecf4b13d8ae469&id=dd2d98db0f" />
         </Container>
       </Section>
 
-      <Section
-        id="trailer"
-        className="has-background lg text-white"
-        style={{
-          backgroundImage: `url(/static/img/screenshots/gameplay_3.png)`
-        }}
-      >
-        <div className="embed-responsive embed-responsive-16by9">
+      <Section id="trailer" className="bg-light lg text-center">
+        <Subtitle>Alpha Gameplay Trailer</Subtitle>
+        <div className="embed-responsive embed-responsive-16by9 rounded">
           <iframe
             className="embed-responsive-item"
             src="https://www.youtube-nocookie.com/embed/SmtEFYRiVrw?rel=0&showinfo=0"
@@ -76,7 +66,7 @@ export default () => {
 
       <Section id="screenshots">
         <Container className="container-narrow text-center">
-          <Subtitle className="mb-3">Screenshots</Subtitle>
+          <Subtitle>Screenshots</Subtitle>
           <ImageGrid images={screenshots} />
         </Container>
       </Section>
