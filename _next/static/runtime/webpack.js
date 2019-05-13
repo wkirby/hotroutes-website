3c0dbb49654db33c5f2b559329792809bbf8f103
Noah Callaway <noah@apsis.io>
2019-05-12 17:46:27 -0700

    Updates

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "fdece0e8aaea8dce34b6";
+/******/ 	var hotCurrentHash = "6cb99072ca2cce621deb";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
---
+++ _next/static/webpack/static/development/pages/support.js.fdece0e8aaea8dce34b6.hot-update.js
@@ -0,0 +1,679 @@
+webpackHotUpdate("static/development/pages/support.js",{
+
+/***/ "./data/screenshots.yml":
+false,
+
+/***/ "./lib/components/ImageGrid.js":
+false,
+
+/***/ "./lib/components/MailChimpForm.js":
+false,
+
+/***/ "./node_modules/@babel/runtime-corejs2/core-js/promise.js":
+false,
+
+/***/ "./node_modules/@babel/runtime-corejs2/helpers/esm/asyncToGenerator.js":
+false,
+
+/***/ "./node_modules/@babel/runtime-corejs2/regenerator/index.js":
+false,
+
+/***/ "./node_modules/core-js/library/fn/promise.js":
+false,
+
+/***/ "./node_modules/core-js/library/modules/_invoke.js":
+false,
+
+/***/ "./node_modules/core-js/library/modules/_microtask.js":
+false,
+
+/***/ "./node_modules/core-js/library/modules/_new-promise-capability.js":
+false,
+
+/***/ "./node_modules/core-js/library/modules/_perform.js":
+false,
+
+/***/ "./node_modules/core-js/library/modules/_promise-resolve.js":
+false,
+
+/***/ "./node_modules/core-js/library/modules/_species-constructor.js":
+false,
+
+/***/ "./node_modules/core-js/library/modules/_task.js":
+false,
+
+/***/ "./node_modules/core-js/library/modules/_user-agent.js":
+false,
+
+/***/ "./node_modules/core-js/library/modules/es6.promise.js":
+false,
+
+/***/ "./node_modules/core-js/library/modules/es7.promise.finally.js":
+false,
+
+/***/ "./node_modules/core-js/library/modules/es7.promise.try.js":
+false,
+
+/***/ "./node_modules/deepmerge/dist/es.js":
+false,
+
+/***/ "./node_modules/formik/dist/formik.esm.js":
+false,
+
+/***/ "./node_modules/hoist-non-react-statics/dist/hoist-non-react-statics.cjs.js":
+false,
+
+/***/ "./node_modules/jsonp/index.js":
+false,
+
+/***/ "./node_modules/jsonp/node_modules/debug/src/browser.js":
+false,
+
+/***/ "./node_modules/jsonp/node_modules/debug/src/debug.js":
+false,
+
+/***/ "./node_modules/jsonp/node_modules/ms/index.js":
+false,
+
+/***/ "./node_modules/lodash-es/_DataView.js":
+false,
+
+/***/ "./node_modules/lodash-es/_Hash.js":
+false,
+
+/***/ "./node_modules/lodash-es/_ListCache.js":
+false,
+
+/***/ "./node_modules/lodash-es/_Map.js":
+false,
+
+/***/ "./node_modules/lodash-es/_MapCache.js":
+false,
+
+/***/ "./node_modules/lodash-es/_Promise.js":
+false,
+
+/***/ "./node_modules/lodash-es/_Set.js":
+false,
+
+/***/ "./node_modules/lodash-es/_Stack.js":
+false,
+
+/***/ "./node_modules/lodash-es/_Symbol.js":
+false,
+
+/***/ "./node_modules/lodash-es/_Uint8Array.js":
+false,
+
+/***/ "./node_modules/lodash-es/_WeakMap.js":
+false,
+
+/***/ "./node_modules/lodash-es/_arrayEach.js":
+false,
+
+/***/ "./node_modules/lodash-es/_arrayFilter.js":
+false,
+
+/***/ "./node_modules/lodash-es/_arrayLikeKeys.js":
+false,
+
+/***/ "./node_modules/lodash-es/_arrayMap.js":
+false,
+
+/***/ "./node_modules/lodash-es/_arrayPush.js":
+false,
+
+/***/ "./node_modules/lodash-es/_assignValue.js":
+false,
+
+/***/ "./node_modules/lodash-es/_assocIndexOf.js":
+false,
+
+/***/ "./node_modules/lodash-es/_baseAssign.js":
+false,
+
+/***/ "./node_modules/lodash-es/_baseAssignIn.js":
+false,
+
+/***/ "./node_modules/lodash-es/_baseAssignValue.js":
+false,
+
+/***/ "./node_modules/lodash-es/_baseClone.js":
+false,
+
+/***/ "./node_modules/lodash-es/_baseCreate.js":
+false,
+
+/***/ "./node_modules/lodash-es/_baseGetAllKeys.js":
+false,
+
+/***/ "./node_modules/lodash-es/_baseGetTag.js":
+false,
+
+/***/ "./node_modules/lodash-es/_baseIsArguments.js":
+false,
+
+/***/ "./node_modules/lodash-es/_baseIsMap.js":
+false,
+
+/***/ "./node_modules/lodash-es/_baseIsNative.js":
+false,
+
+/***/ "./node_modules/lodash-es/_baseIsSet.js":
+false,
+
+/***/ "./node_modules/lodash-es/_baseIsTypedArray.js":
+false,
+
+/***/ "./node_modules/lodash-es/_baseKeys.js":
+false,
+
+/***/ "./node_modules/lodash-es/_baseKeysIn.js":
+false,
+
+/***/ "./node_modules/lodash-es/_baseTimes.js":
+false,
+
+/***/ "./node_modules/lodash-es/_baseToString.js":
+false,
+
+/***/ "./node_modules/lodash-es/_baseUnary.js":
+false,
+
+/***/ "./node_modules/lodash-es/_cloneArrayBuffer.js":
+false,
+
+/***/ "./node_modules/lodash-es/_cloneBuffer.js":
+false,
+
+/***/ "./node_modules/lodash-es/_cloneDataView.js":
+false,
+
+/***/ "./node_modules/lodash-es/_cloneRegExp.js":
+false,
+
+/***/ "./node_modules/lodash-es/_cloneSymbol.js":
+false,
+
+/***/ "./node_modules/lodash-es/_cloneTypedArray.js":
+false,
+
+/***/ "./node_modules/lodash-es/_copyArray.js":
+false,
+
+/***/ "./node_modules/lodash-es/_copyObject.js":
+false,
+
+/***/ "./node_modules/lodash-es/_copySymbols.js":
+false,
+
+/***/ "./node_modules/lodash-es/_copySymbolsIn.js":
+false,
+
+/***/ "./node_modules/lodash-es/_coreJsData.js":
+false,
+
+/***/ "./node_modules/lodash-es/_defineProperty.js":
+false,
+
+/***/ "./node_modules/lodash-es/_freeGlobal.js":
+false,
+
+/***/ "./node_modules/lodash-es/_getAllKeys.js":
+false,
+
+/***/ "./node_modules/lodash-es/_getAllKeysIn.js":
+false,
+
+/***/ "./node_modules/lodash-es/_getMapData.js":
+false,
+
+/***/ "./node_modules/lodash-es/_getNative.js":
+false,
+
+/***/ "./node_modules/lodash-es/_getPrototype.js":
+false,
+
+/***/ "./node_modules/lodash-es/_getRawTag.js":
+false,
+
+/***/ "./node_modules/lodash-es/_getSymbols.js":
+false,
+
+/***/ "./node_modules/lodash-es/_getSymbolsIn.js":
+false,
+
+/***/ "./node_modules/lodash-es/_getTag.js":
+false,
+
+/***/ "./node_modules/lodash-es/_getValue.js":
+false,
+
+/***/ "./node_modules/lodash-es/_hashClear.js":
+false,
+
+/***/ "./node_modules/lodash-es/_hashDelete.js":
+false,
+
+/***/ "./node_modules/lodash-es/_hashGet.js":
+false,
+
+/***/ "./node_modules/lodash-es/_hashHas.js":
+false,
+
+/***/ "./node_modules/lodash-es/_hashSet.js":
+false,
+
+/***/ "./node_modules/lodash-es/_initCloneArray.js":
+false,
+
+/***/ "./node_modules/lodash-es/_initCloneByTag.js":
+false,
+
+/***/ "./node_modules/lodash-es/_initCloneObject.js":
+false,
+
+/***/ "./node_modules/lodash-es/_isIndex.js":
+false,
+
+/***/ "./node_modules/lodash-es/_isKeyable.js":
+false,
+
+/***/ "./node_modules/lodash-es/_isMasked.js":
+false,
+
+/***/ "./node_modules/lodash-es/_isPrototype.js":
+false,
+
+/***/ "./node_modules/lodash-es/_listCacheClear.js":
+false,
+
+/***/ "./node_modules/lodash-es/_listCacheDelete.js":
+false,
+
+/***/ "./node_modules/lodash-es/_listCacheGet.js":
+false,
+
+/***/ "./node_modules/lodash-es/_listCacheHas.js":
+false,
+
+/***/ "./node_modules/lodash-es/_listCacheSet.js":
+false,
+
+/***/ "./node_modules/lodash-es/_mapCacheClear.js":
+false,
+
+/***/ "./node_modules/lodash-es/_mapCacheDelete.js":
+false,
+
+/***/ "./node_modules/lodash-es/_mapCacheGet.js":
+false,
+
+/***/ "./node_modules/lodash-es/_mapCacheHas.js":
+false,
+
+/***/ "./node_modules/lodash-es/_mapCacheSet.js":
+false,
+
+/***/ "./node_modules/lodash-es/_memoizeCapped.js":
+false,
+
+/***/ "./node_modules/lodash-es/_nativeCreate.js":
+false,
+
+/***/ "./node_modules/lodash-es/_nativeKeys.js":
+false,
+
+/***/ "./node_modules/lodash-es/_nativeKeysIn.js":
+false,
+
+/***/ "./node_modules/lodash-es/_nodeUtil.js":
+false,
+
+/***/ "./node_modules/lodash-es/_objectToString.js":
+false,
+
+/***/ "./node_modules/lodash-es/_overArg.js":
+false,
+
+/***/ "./node_modules/lodash-es/_root.js":
+false,
+
+/***/ "./node_modules/lodash-es/_stackClear.js":
+false,
+
+/***/ "./node_modules/lodash-es/_stackDelete.js":
+false,
+
+/***/ "./node_modules/lodash-es/_stackGet.js":
+false,
+
+/***/ "./node_modules/lodash-es/_stackHas.js":
+false,
+
+/***/ "./node_modules/lodash-es/_stackSet.js":
+false,
+
+/***/ "./node_modules/lodash-es/_stringToPath.js":
+false,
+
+/***/ "./node_modules/lodash-es/_toKey.js":
+false,
+
+/***/ "./node_modules/lodash-es/_toSource.js":
+false,
+
+/***/ "./node_modules/lodash-es/clone.js":
+false,
+
+/***/ "./node_modules/lodash-es/cloneDeep.js":
+false,
+
+/***/ "./node_modules/lodash-es/eq.js":
+false,
+
+/***/ "./node_modules/lodash-es/isArguments.js":
+false,
+
+/***/ "./node_modules/lodash-es/isArray.js":
+false,
+
+/***/ "./node_modules/lodash-es/isArrayLike.js":
+false,
+
+/***/ "./node_modules/lodash-es/isBuffer.js":
+false,
+
+/***/ "./node_modules/lodash-es/isFunction.js":
+false,
+
+/***/ "./node_modules/lodash-es/isLength.js":
+false,
+
+/***/ "./node_modules/lodash-es/isMap.js":
+false,
+
+/***/ "./node_modules/lodash-es/isObject.js":
+false,
+
+/***/ "./node_modules/lodash-es/isObjectLike.js":
+false,
+
+/***/ "./node_modules/lodash-es/isSet.js":
+false,
+
+/***/ "./node_modules/lodash-es/isSymbol.js":
+false,
+
+/***/ "./node_modules/lodash-es/isTypedArray.js":
+false,
+
+/***/ "./node_modules/lodash-es/keys.js":
+false,
+
+/***/ "./node_modules/lodash-es/keysIn.js":
+false,
+
+/***/ "./node_modules/lodash-es/memoize.js":
+false,
+
+/***/ "./node_modules/lodash-es/stubArray.js":
+false,
+
+/***/ "./node_modules/lodash-es/stubFalse.js":
+false,
+
+/***/ "./node_modules/lodash-es/toPath.js":
+false,
+
+/***/ "./node_modules/lodash-es/toString.js":
+false,
+
+/***/ "./node_modules/process/browser.js":
+false,
+
+/***/ "./node_modules/react-fast-compare/index.js":
+false,
+
+/***/ "./node_modules/react-mailchimp-subscribe/es/SimpleForm.js":
+false,
+
+/***/ "./node_modules/react-mailchimp-subscribe/es/index.js":
+false,
+
+/***/ "./node_modules/react-medium-image-zoom/lib/EventsWrapper.js":
+false,
+
+/***/ "./node_modules/react-medium-image-zoom/lib/ImageZoom.js":
+false,
+
+/***/ "./node_modules/react-medium-image-zoom/lib/Overlay.js":
+false,
+
+/***/ "./node_modules/react-medium-image-zoom/lib/Zoom.js":
+false,
+
+/***/ "./node_modules/react-medium-image-zoom/lib/defaults.js":
+false,
+
+/***/ "./node_modules/react-medium-image-zoom/lib/helpers.js":
+false,
+
+/***/ "./node_modules/react-medium-image-zoom/lib/index.js":
+false,
+
+/***/ "./node_modules/react-medium-image-zoom/lib/keyboardEvents.js":
+false,
+
+/***/ "./node_modules/regenerator-runtime/runtime-module.js":
+false,
+
+/***/ "./node_modules/regenerator-runtime/runtime.js":
+false,
+
+/***/ "./node_modules/tiny-warning/dist/tiny-warning.esm.js":
+false,
+
+/***/ "./node_modules/to-querystring/lib/to-querystring.min.js":
+false,
+
+/***/ "./node_modules/tslib/tslib.es6.js":
+false,
+
+/***/ "./node_modules/webpack/buildin/harmony-module.js":
+false,
+
+/***/ "./pages/support.js":
+/*!**************************!*\
+  !*** ./pages/support.js ***!
+  \**************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var reactstrap__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! reactstrap */ "./node_modules/reactstrap/es/index.js");
+/* harmony import */ var _lib_components_Hero__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../lib/components/Hero */ "./lib/components/Hero.js");
+/* harmony import */ var _lib_components_Layout__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../lib/components/Layout */ "./lib/components/Layout.js");
+/* harmony import */ var _lib_components_Section__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ../lib/components/Section */ "./lib/components/Section.js");
+/* harmony import */ var _lib_components_Title__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ../lib/components/Title */ "./lib/components/Title.js");
+var _jsxFileName = "/Users/noah/dev/hotroutes-website/pages/support.js";
+
+
+
+
+
+
+/* harmony default export */ __webpack_exports__["default"] = (function () {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_lib_components_Layout__WEBPACK_IMPORTED_MODULE_3__["Layout"], {
+    pageTitle: "Welcome",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 11
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_lib_components_Hero__WEBPACK_IMPORTED_MODULE_2__["Hero"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 12
+    },
+    __self: this
+  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_lib_components_Section__WEBPACK_IMPORTED_MODULE_4__["Section"], {
+    id: "copy",
+    className: "section--football-helmet",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 13
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(reactstrap__WEBPACK_IMPORTED_MODULE_1__["Container"], {
+    className: "container-narrow text-center",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 14
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_lib_components_Title__WEBPACK_IMPORTED_MODULE_5__["Subtitle"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 15
+    },
+    __self: this
+  }, "Having Problems?"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    className: "lead",
+    style: {
+      textAlign: 'justify'
+    },
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 16
+    },
+    __self: this
+  }, "We're here to help. We want everyone to enjoy the experience, so if something's gone wrong, we'll try get everything back on track."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    className: "lead",
+    style: {
+      textAlign: 'justify'
+    },
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 21
+    },
+    __self: this
+  }, "You can either reach out to us on our discord below, or if you'd prefer use the contact form to send us an e-mail and we'll follow up that way."))), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_lib_components_Section__WEBPACK_IMPORTED_MODULE_4__["Section"], {
+    id: "follow",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 28
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(reactstrap__WEBPACK_IMPORTED_MODULE_1__["Container"], {
+    className: "container text-center",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 29
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_lib_components_Title__WEBPACK_IMPORTED_MODULE_5__["Subtitle"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 30
+    },
+    __self: this
+  }, "Choose Wisely"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "row",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 32
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "col-lg-5",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 33
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    class: "lead",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 34
+    },
+    __self: this
+  }, "Message us on discord"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("a", {
+    className: "logo-link",
+    href: "https://discord.gg/5WNHbpd",
+    target: "_blank",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 35
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
+    className: "img-fluid",
+    src: "/static/img/logos/discord.svg",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 36
+    },
+    __self: this
+  }))), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("hr", {
+    className: "my-5 d-xs-block d-lg-none",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 40
+    },
+    __self: this
+  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "col-lg-5 offset-lg-2",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 42
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    class: "lead",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 43
+    },
+    __self: this
+  }, "Or fill out our cool form"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("script", {
+    type: "text/javascript",
+    src: "https://s3.amazonaws.com/assets.freshdesk.com/widget/freshwidget.js",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 44
+    },
+    __self: this
+  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("style", {
+    type: "text/css",
+    media: "screen, projection",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 45
+    },
+    __self: this
+  }, "@import url(https://s3.amazonaws.com/assets.freshdesk.com/widget/freshwidget.css);"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("iframe", {
+    title: "Feedback Form",
+    class: "freshwidget-embedded-form",
+    id: "freshwidget-embedded-form",
+    src: "https://ncallaway.freshdesk.com/widgets/feedback_widget/new?&widgetType=embedded&submitThanks=Thank+you+for+your+message!+We're+sorry+you're+having+issues+and+we'll+be+in+touch+shortly.&screenshot=No&searchArea=no&captcha=yes",
+    scrolling: "no",
+    height: "500px",
+    width: "100%",
+    frameborder: "0",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 48
+    },
+    __self: this
+  }))))));
+});
+
+/***/ })
+
+})
+//# sourceMappingURL=support.js.fdece0e8aaea8dce34b6.hot-update.js.map
