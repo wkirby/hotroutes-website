b81af19cac8af9d5ed276982e5f6fb2661a1959e
Noah Callaway <noah@apsis.io>
2019-05-12 17:43:49 -0700

    Update support page

--- pages/support.js
+++ pages/support.js
@@ -34,23 +34,23 @@ export default () => {
           <Subtitle>Choose Wisely</Subtitle>
 
           <div className="row">
-            <div className="col-sm-5">
-              <p class="lead">Fill out our cool form</p>
-              <script type="text/javascript" src="https://s3.amazonaws.com/assets.freshdesk.com/widget/freshwidget.js"></script>
-              <style type="text/css" media="screen, projection">
-                @import url(https://s3.amazonaws.com/assets.freshdesk.com/widget/freshwidget.css);
-              </style>
-              <iframe title="Feedback Form" class="freshwidget-embedded-form" id="freshwidget-embedded-form" src="https://ncallaway.freshdesk.com/widgets/feedback_widget/new?&widgetType=embedded&submitThanks=Thank+you+for+your+feedback!+We'll+be+in+touch+shortly.+You+might+be+able+to+get+&screenshot=No&searchArea=no&captcha=yes" scrolling="no" height="500px" width="100%" frameborder="0" >
-              </iframe>
-            </div>
-
-            <hr className="my-4 d-xs-block d-sm-none" />
-
-            <div className="col-sm-5 offset-sm-2">
+            <div className="col-lg-5">
               <p class="lead">Message us on discord</p>
               <a className="logo-link" href="https://discord.gg/5WNHbpd" target="_blank">
                 <img className="img-fluid" src="/static/img/logos/discord.svg" />
               </a>
+            </div>
+
+            <hr className="my-5 d-xs-block d-lg-none" />
+
+            <div className="col-lg-5 offset-lg-2">
+              <p class="lead">Or fill out our cool form</p>
+              <script type="text/javascript" src="https://s3.amazonaws.com/assets.freshdesk.com/widget/freshwidget.js"></script>
+              <style type="text/css" media="screen, projection">
+                @import url(https://s3.amazonaws.com/assets.freshdesk.com/widget/freshwidget.css);
+              </style>
+              <iframe title="Feedback Form" class="freshwidget-embedded-form" id="freshwidget-embedded-form" src="https://ncallaway.freshdesk.com/widgets/feedback_widget/new?&widgetType=embedded&submitThanks=Thank+you+for+your+message!+We're+sorry+you're+having+issues+and+we'll+be+in+touch+shortly.&screenshot=No&searchArea=no&captcha=yes" scrolling="no" height="500px" width="100%" frameborder="0" >
+              </iframe>
             </div>
           </div>
 
